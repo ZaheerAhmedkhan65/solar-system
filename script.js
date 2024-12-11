e9697909670e941ecff58e1408c59b3d59d99186
ZaheerAhmedkhan65 <ranazaheerahmed65@gmail.com>
2024-12-11 16:06:12 +0500

    completed solar system

---
+++ script.js
@@ -0,0 +1,12 @@
+let num = 255;
+for(var i=1;i<num;i++){
+    let star = document.createElement('div');
+    star.style.position = 'absolute';
+    star.style.left = Math.random() * window.innerWidth + 'px';
+    star.style.top = Math.random() * window.innerHeight + 'px';
+    star.style.width = Math.random() * 4 + 'px';
+    star.style.height = Math.random() * 4 + 'px';
+    star.style.background = 'white';
+    star.style.borderRadius = '50%';
+    document.body.appendChild(star);
+}
